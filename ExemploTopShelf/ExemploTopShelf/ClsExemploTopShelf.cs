ab1cc4fcea2f45190d9779d2a93fee1ccea4eb68
Ricardo <hick.hk@gmail.com>
2021-03-09 13:37:06 -0300

    c0 - primeiro commit

---
+++ ExemploTopShelf/ExemploTopShelf/ClsExemploTopShelf.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ExemploTopShelf
+{
+    class ClsExemploTopShelf
+    {
+        private Timer timer;
+
+        public ClsExemploTopShelf()
+        {
+            timer = new Timer(10000);
+
+            timer.Elapsed += Timer_Elipsed;
+
+        }
+
+        private void Timer_Elipsed(object sender, ElapsedEventArgs e)
+        {
+            string mensagem = $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} teste";
+
+            Console.WriteLine(mensagem);
+
+            using (StreamWriter arquivo = new StreamWriter(@"D:\Projetos\TopShelf c#\ExemploTopShelf\teste.txt",true))
+            {
+                arquivo.WriteLine(mensagem);
+                arquivo.Close();
+            }
+
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
---
+++ ExemploTopShelf/ExemploTopShelf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ExemploTopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run( s => 
+            {
+                s.Service<ClsExemploTopShelf>(m =>
+                {
+                    m.ConstructUsing(st => new ClsExemploTopShelf());
+                    m.WhenStarted(st => st.Start());
+                    m.WhenStopped(st => st.Stop());
+                });
+
+                s.RunAsLocalService();
+
+                s.SetDescription("Exemplo de serviço utilizando o topshelf.");
+                s.SetDisplayName("Exemplo TopShelf");
+                s.SetServiceName("ExemploTopShelf");
+            });
+        }
+    }
+}
